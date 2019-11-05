00ca402f14e3bc3153f98094810f9e9ac2eaf975
Wendy Bartos <wendyebartos@gmail.com>
2019-11-05 14:42:05 -0800

    adding index.js to master

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import api from './api';
 function main() {
   api.getItems()
     .then(res => console.log(res));  
-  console.log(api.BASE_URL);
-
-  const startMsg = $('<p>Webpack is working!</p>');
-  $('#root').append(startMsg);
+  
 }
 
 $(main);
